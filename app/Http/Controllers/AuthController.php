dd55d4b9db4d29320ef796b7fec924f1a8165287
sawczuk24427 <sawczuk24427@stud.akademiabialska.pl>
2026-07-06 10:40:24 +0200

    fix: added imports to AuthController and authorized LoginRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
