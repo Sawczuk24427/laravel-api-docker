754815fa7abb1e3ce0e7f3e42b2d9b810391c4df
sawczuk24427 <sawczuk24427@stud.akademiabialska.pl>
2026-07-10 13:46:32 +0200

    feat: added register route and grouped auth routes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::post('/login', [AuthController::class, 'login']);
 
-
-
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
