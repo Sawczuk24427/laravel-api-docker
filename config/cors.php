0c41457fa4611b57708ff0abb7191125138c4c05
sawczuk24427 <sawczuk24427@stud.akademiabialska.pl>
2026-07-15 10:48:31 +0200

    hotfix: added auth route group to cors config

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
 
     'allowed_methods' => ['*'],
 
