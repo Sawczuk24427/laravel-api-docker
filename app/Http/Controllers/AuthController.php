a1fbb99809bf4540bf551690cab1984e2ca293d6
sawczuk24427 <sawczuk24427@stud.akademiabialska.pl>
2026-07-15 10:51:21 +0200

    hotfix: added register request to controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
