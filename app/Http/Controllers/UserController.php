358a024e32cac85919af083b3d9bf6a2554ebe5e
sawczuk24427 <sawczuk24427@stud.akademiabialska.pl>
2026-07-27 12:15:28 +0200

    feat: added routing and controller for user crud operations

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\StoreUserRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+        $users = User::all();
+        return UserResource::collection($users);
+    }
+
+    public function show(User $user){
+        
+        return new UserResource($user);
+    }
+
+    public function store(StoreUserRequest $request){
+        $validated = $request->validated();
+        $user = User::create($validated);
+        return new UserResource($user);
+    }
+
+    public function update(UpdateUserRequest $request, User $user){
+        $user -> update($request->validated());
+        return new UserResource($user);
+    }
+
+    public function destroy(User $user){
+        $user -> delete();
+        return response()->json([null, 204]);
+    }
+}
---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'sometimes',
+                'string',
+                Rule::unique('users')->ignore($this->user)
+            ],
+            'email' => [
+                'sometimes',
+                'email',
+                Rule::unique('users')->ignore($this->user)
+            ],
+            'password'=> ['sometimes', 'string']
+        ];
+    }
+}
